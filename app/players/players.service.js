3f485ab1fbfe5a35f95ef8b1ce803a9578b6b82d
Anita Perala <anitaperala@gmail.com>
2015-11-25 19:44:07 -0500

    anon auth [wip]

---
+++ app/players/players.service.js
@@ -0,0 +1,11 @@
+angular.module('flagMatchApp')
+  .factory('Players', function ($firebaseArray, $firebaseObject, FirebaseUrl){
+    var playersRef = new Firebase(FirebaseUrl+'players');
+    var players = $firebaseArray(playersRef);
+
+    var Players = {};
+
+    return Players;
+  })
+
+  
