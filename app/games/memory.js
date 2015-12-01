f4d1e45c3c416c196b62d175c205c76a0b629b55
Anita Perala <anitaperala@gmail.com>
2015-12-01 18:04:12 -0500

    Add basic css, setup game board

--- app/games/memory.js
+++ app/games/memory.js
@@ -5,9 +5,9 @@ var memoryGame = angular.module('memoryGame', ['flagMatchApp', 'underscore']);
 memoryGame.controller('MemoryGameCtrl', ['$scope', 'Countries', '_',
   function ($scope, Countries, _) {
     Countries.query(function (data) {
-      $scope.random_countries = _.sample(data, 16);
-      $scope.orderProp = 'name';
+      $scope.random_countries = _.sample(data, 15);
     });
   }
 
-  ])
+  ]); //end controller
+
